96f52657148e18fcc89e08233e4583593a2977b7
Sohag Desai <sohag.desai@gmail.com>
2014-08-07 15:49:42 -0700

    final version of plot1.R - reads entire dataset

--- plot1.R
+++ plot1.R
@@ -1,7 +1,22 @@
 library(graphics)
 plot1 <- function() {
-    hpcplotdata <- read.csv("household_power_consumption_subset.txt",sep=";")
+    # Read in data
+    hpcdata <- read.csv("household_power_consumption.txt",sep=";")
+
+    # Subset the data to lie between the two days of interest
+    hpcdata1 <- subset(hpcdata[as.Date(hpcdata$Date,format = "%d/%m/%Y") 
+                >= as.Date("2007-02-01") & 
+                as.Date(hpcdata$Date,format = "%d/%m/%Y") 
+                <= as.Date("2007-02-02") ,])
+
+    # Set up the PNG file to write to
     png(filename = "plot1.png")
-    hist(hpcplotdata$Global_active_power,xlab="Global Active Power (kilowatts)", main = "Global Active Power",col="red")
+
+    # Plot histogram
+    hist(as.numeric(as.character(hpcdata1$Global_active_power)),
+         xlab="Global Active Power (kilowatts)", 
+         main = "Global Active Power",col="red")
+
+    # Turn off the device
     dev.off()
 }
