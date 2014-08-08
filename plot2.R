226cdc19c1670d88f10ea9c0b31134c5428f9ceb
Sohag Desai <sohag.desai@gmail.com>
2014-08-07 18:36:02 -0700

    Updated plot2.R and plot2.png

--- plot2.R
+++ plot2.R
@@ -1,10 +1,33 @@
 library(graphics)
 plot2 <- function() {
-        hpcplotdata <- read.csv("household_power_consumption_subset.txt",sep=";")
+        # Read in data
+        hpcdata <- read.csv("household_power_consumption.txt",sep=";")
+        
+        # Subset the data to lie between the two days of interest
+        hpcdata1 <- subset(hpcdata[as.Date(hpcdata$Date,format = "%d/%m/%Y") 
+                                   >= as.Date("2007-02-01") & 
+                                           as.Date(hpcdata$Date,format = "%d/%m/%Y") 
+                                   <= as.Date("2007-02-02") ,])
+        
+        
+        # Constuct a column with Data and Time columns merged together
+        xdata <- paste(as.Date(hpcdata1$Date, format="%d/%m/%Y"),
+                       hpcdata1$Time)
+        
+        # Insert this column into the data frame so that strptime()
+        # can be used on this new column
+        hpcdata1 <- cbind("Date_and_Time" = xdata, hpcdata1)
+        
+        # Set up the PNG file to write to
         png(filename = "plot2.png")
-        plot(hpcplotdata$Time,hpcplotdata$Global_active_power,ylab="Global Active Power (kilowatts)",xaxt="n")
-        v1 <- c(1,1440,2880)
-        v2 <- c("Thu","Fri","Sat")
-        axis(side = 1, at = v1, labels = v2)
+        
+        # Plot the data as a line chart (type = "l")
+        plot(strptime(hpcdata1$Date_and_Time, "%Y-%m-%d %H:%M:%S"),
+             as.numeric(as.character(hpcdata1$Global_active_power)),
+             xlab = "", 
+             ylab = "Global Active Power (kilowatts)", 
+             type = "l") 
+ 
+        # Turn off the device
         dev.off()
 }
